d246a438bcf91215fbfe4267cbd2c5a2375565cf
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-08 14:12:01 +0100

    api with forecast data added

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function search(city) {
 
 search("Krakow");
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
 let forecastElement = document.querySelector(".forecast");
 let forecastHTML =`<div class="row">`;
 let days = [
@@ -40,7 +41,12 @@ forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
+
+function getForecast(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=e6c2364656962bdcb16bc352fc42569a&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 
 
 function showT(response) {
@@ -60,6 +66,8 @@ function showT(response) {
   current.innerHTML = `${response.data.name}`;
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt",response.data.weather[0].description);
+  
+  getForecast(response.data.coord);
 }
 
 function showPosition(position) {
